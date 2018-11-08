6ce3b0193f9fe786dc94b094f688532c178dbf0b
simodecl <simodecl@student.arteveldehs.be>
2018-11-08 16:30:50 +0100

    yoinkies

--- angular/src/app/components/student-detail/student-detail.component.ts
+++ angular/src/app/components/student-detail/student-detail.component.ts
@@ -1,3 +1,4 @@
+import { Course } from './../../models/course';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudentService } from './../../services/student.service';
@@ -12,6 +13,7 @@ import { JsonObject } from './../../models/json-object';
 export class StudentDetailComponent implements OnInit {
 
   public student: Student;
+  public courses: Course[];
 
   constructor(
     private studentService: StudentService,
@@ -28,6 +30,7 @@ export class StudentDetailComponent implements OnInit {
       const id = this.route.snapshot.paramMap.get('id');
       const jsonResponse = await this.studentService.getStudent<JsonObject>(id);
       this.student = jsonResponse.data;
+      jsonResponse.included ? this.courses = jsonResponse.included : console.log('No courses found');
       console.log(jsonResponse);
     } catch ( error ) {
       console.error( error );
